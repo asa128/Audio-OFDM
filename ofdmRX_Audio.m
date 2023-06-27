cb3339a7a3ed373fc2058cac9af477674aea47fd
asa128 <77521902+asa128@users.noreply.github.com>
2023-06-26 23:36:32 -0500

    Added comments to code
    Added comments to code to improve readability. Also added titles, labels, and legends to plots.

--- ofdmRX_Audio.m
+++ ofdmRX_Audio.m
@@ -1,58 +1,23 @@
+%% OFDM DECODER
 
-
-% ofdm decoder
-clear all
+clear
 close all
-clc
-plotcrap = 1;
 
+% PROMPT USER TO SELECT A WAV FILE
 FILTERSPEC = '.wav';
 TITLE = 'Pick a Recorded OFDM IQ wav file';
-FILE = 'D:\gnu_radio_work';
-
+FILE = 'ofdmtest256Char8khzAudio';
 [FILENAME, PATHNAME, FILTERINDEX] = uigetfile(FILTERSPEC, TITLE, FILE);
 xstring = [PATHNAME FILENAME];
-
-[x,fswav] = audioread(xstring);
-
-% tx bandwidth
-
-fs = 8e3;  % sample rate of ofdm signal 
-% pulse at start for sync 
-N = 8000;  %number of samples for chirp sync pulse
-t = [0:7999]/fs;
-f0 = 300;
-f1 = 3000;
-T = 1;
-c = (f1 - f0)/T;
-x1 = (c/2)*t.^2 + f0*t;
-pream = sin(2*pi*x1);
-
-Nguard = 512;   % time between pulse and preamble fft data
-
-Nchar = 256;  % make power of 2
-bits = Nchar * 8;
-N2 = bits/2;
-Nfft = bits + 1024;
-Nfft = (2*Nfft)  - 1 ; 
+[x,fswav] = audioread(xstring);  % read in file and its sample rate
 
 
+x = x(:,1);  % if audio is multichannel, discard all but first channel
+x = x.';  % convert column vector of audio samples to row vector
 
 
-sigtime = Nguard + 2*Nfft + 2*Nfft + 2*Nguard;
-
-
-
-
-
-x = x(:,1);
-x = x.';
-
-
-
-
-
-
+% RESAMPLE VECTOR OF AUDIO SAMPLES, IF NECESSARY
+fs = 8e3;  % sample rate of ofdm signal
 if fswav ~= fs
     n = gcd(fs,fswav);
     p = fs / n;
@@ -61,141 +26,162 @@ if fswav ~= fs
 end
 
 
-hpw = conj(pream(end:-1:1));
-xdet = filter(hpw,1,x);
+% GENERATE CHIRP SIGNAL FOR SYNCHRONIZATION
+N = 8000;  % number of samples for chirp
+t = (0:7999)/fs;  % time samples for chirp
+f0 = 300;
+f1 = 3000;
+% f_i = f0 + (f1-f0)*t;  % instantaneous frequency of chirp
+pha_i = 2*pi*f0*t + pi*(f1-f0)*t.^2;  % instantaneous phase of chirp (2pi times integral of instantaneous freq)
+chirp = sin(pha_i);  % generate chirp
 
-if plotcrap
-    figure(100)
-    plot(xdet)
-    title('chirp detection')
-end
+Nguard = 512;   % time between pulse and preamble fft data
 
-[maxv, maxi] = max(abs(xdet));
+% DEFINE VARIABLES
+Nchar = 256;  % number of characters, make power of 2
+Nbits = Nchar * 8;  % number of bits
+Nfft_2 = Nbits + 1024;  % half of length of FFT, bits plus guard freq bins
+Nfft = (2*Nfft_2)  - 1 ;  % actual length of FFT, due to conjugate symmetry
+
+
+sigtime = Nguard + 2*Nfft + 2*Nfft + 2*Nguard;  % time duration, in samples, of desired signal
+
+
+% UTILIZE CHIRP FOR SYNCHRONIZATION
+hpw = conj(chirp(end:-1:1)); % time reverse and conjugate chirp sync pulse
+xdet = conv(hpw,x);  %convolve signal with reverse-conjugated chirp
+
+% PLOT SIGNAL CONVOLVED WITH REVERSE-CONJUGATE CHIRP
+figure(1)
+plot(xdet)
+title('chirp detection')
+xlabel('sample')
+ylabel('x[n] * chirp*[n]')
+
+
+[maxv, maxi] = max(abs(xdet));  % determine max value and index from detection signal
+
+
+% determine start and end samples for desired portion of audio
 xstart = maxi ;
 xend = xstart + sigtime ;
 
-if plotcrap
-    figure(101)
-    xplot = abs(x) / max(abs(x));
-    xplot = 20 * log10(xplot);
-    yplot = length(xdet);
-    yplot = zeros(1,yplot);
-    yplot(maxi) = -40;
-    plot(xplot)
-    hold on
-    plot(yplot)
-    hold off
-end
+
+% PLOT SIGNAL AND DETECTED CHIRP END LOCATION
+figure(2)
+yplot = zeros(1,length(xdet));
+yplot(maxi) = 2;
+plot(x)
+hold on
+plot(yplot)
+hold off
+title('Detected end location of chirp')
+xlabel('sample')
+ylabel('x[n]')
+legend('signal','detected chirp end location')
 
 
+x = x(xstart:xend);  % obtain desired portion of audio
 
 
-
-x = x(xstart:xend);
-a = Nguard  + round(0.8*Nfft) + 1 ;
-b = a + Nfft   - 1 ;
-xp = x(a:b);
-
-a1 =  Nguard + 2*Nfft + round(0.8*Nfft) + 1 ;
-b2 = a1 + Nfft  - 1 ;
-xd = x(a1:b2);
+% EXTRACT PILOT TIME SIGNAL
+xp0 = Nguard  + round(0.8*Nfft) + 1;  % start sample for pilot
+xp1 = xp0 + Nfft - 1;  % end sample for pilot
+xp = x(xp0:xp1); % extract pilot from audio
 
 
+% EXTRACT DATA TIME SIGNAL
+xd0 =  Nguard + 2*Nfft + round(0.8*Nfft) + 1;  % start sample for data
+xd1 = xd0 + Nfft - 1;  % end sample for data
+xd = x(xd0:xd1);  % extract data from audio
 
 
+% PLOT EXTRACTED SIGNALS
+figure(3)
+x_plot = x;
+xp_plot = zeros(1,length(x));
+xp_plot(xp0:xp1) = 1;  % window for pilot FFT
+xd_plot = zeros(1,length(x));
+xd_plot(xd0:xd1) = 1;  % window for data FFT
+plot(x_plot);
+hold on
+plot(xp_plot);
+plot(xd_plot);
+hold off
+title('Audio samples after chirp detection pulse')
+xlabel('Sample')
+ylabel('x[n]')
+legend('received signal','portion used for pilot FFT', 'portion used for data FFT')
 
 
-
+% COMPUTE PILOT AND DATA SIGNAL FOURIER COEFFICIENTS WITH FFT
 Xp = fft(xp);
-Xp = Xp( 1:floor(end/2) );
+Xp = Xp( 1:floor(end/2) );  % keep only first half of Fourier coefficients
+
 Xd = fft(xd);
-Xd = Xd( 1:floor(end/2) );
+Xd = Xd( 1:floor(end/2) );  % keep only first half of Fourier coefficients
 
-if plotcrap
-    n = length(Xd);
-    fvec = linspace(0,fs/2,n);
-    figure(103)
-    plot(fvec,20*log10(abs(Xp)))
-    hold on
-    plot(fvec,20*log10(abs(Xd)))
-    legend('Pilot FFT', 'Data FFT');
-    hold off
+
+% PLOT PILOT AND DATA SPECTRA
+n = length(Xd);
+fvec = linspace(0,fs/2,n);
+figure(4)
+plot(fvec,20*log10(abs(Xp)))
+hold on
+plot(fvec,20*log10(abs(Xd)))
+hold off
+title('Pilot and data spectra')
+xlabel('frequency')
+ylabel('dB')
+legend('Pilot FFT', 'Data FFT');
+
+
+% OBTAIN PHASE FOR EACH BIT
+det = abs( angle(Xp ./ Xd) );  % compute absolute phase difference between pilot and data Fourier coefficients
+
+det = det(513:end);  % discard first 512 decoded bits (corresponding to nulled subcarriers)
+det = det(1:Nbits);  % keep only bits that were transmitted
+
+
+% PLOT DECODED BIT PHASES
+figure(5)
+plot(det)
+title('demodulate phase vector')
+xlabel('bit number')
+ylabel('bit phase')
+
+
+% DETERMINE BIT VALUES BASED ON THRESHOLD
+thres = pi/2;  % threshold angle for bit determination
+
+det(det <= thres) = 1;  % 1 bits have no phase inversion (i.e., angle less than threshold)
+det(det > thres) = 0;  % 0 bits have phase inversion (i.e., angle greater than threshold)
+
+
+% PLOT DECODED BITS
+figure(6)
+plot(det)
+title('decoded bits vector')
+xlabel('bit number')
+ylabel('bit value')
+
+
+% CONVERT BINARY BACK TO CHARACTERS
+det = reshape(det,8,[]);  % reshape decoded bits into array with columns of 8
+det = det';  % transpose decoded bit array into rows of 8
+
+charmes = zeros(1,Nchar);  % array to store characters
+
+w = 2.^(7:-1:0);  % powers of 2 to convert binary to decimal
+
+for k = 1:Nchar  % loop over each element of character array
+    x = det(k,:);  % extract corresponding row in bit array
+    x = sum(x .* w);  % compute decimal value of the character
+    charmes(k) = x;  % store to kth element of character array
 end
 
 
-
-
-
-
-
-
-
-
-det = angle(Xp ./ Xd);          % ratio to get delta phase diff
-det = abs(det);
-
-thres = pi/2;
-
-% det = ( det <= thres) .* 1   +  (det > thres) .* 0;
-det = det(513:end);
-det = det(1:bits);   % bits for char  
-
-if plotcrap
-    figure(104)
-    plot(det)
-    title('demodulate phase vector')
-end
-
-
-
-
-
-det(det <= thres) = 1;
-det(det > thres) = 0;
-
-if plotcrap
-    figure(105)
-    plot(det)
-    title('decoded bits vector')
-end
-
-
-det = reshape(det,8,[]);
-det = det';
-
-
-charmes = zeros(1,Nchar);
-w = [7:-1:0];
-w = 2.^w;
-
-
-for k = 1:Nchar
-    x = det(k,:);
-    x = x .* w;
-    x = sum(x);
-    charmes(k) = x;
-end
-
-
-
-    clc
-
-   xstr =  char(charmes)
-
-   msgbox(xstr,'replace')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% DISPLAY RESULTS
+clc  % clear command window
+xstr = char(charmes)  % display decoded message in command window
+msgbox(xstr,'replace')  % display decoded message in message box
--- ofdmTX_audio.m
+++ ofdmTX_audio.m
@@ -1,44 +1,42 @@
+%% OFDM ENCODER
 
-
-clear all
+clear
 close all
 
 fs = 8e3;  % sample rate of ofdm signal 
-% pulse at start for sync 
-N = 8000;  %number of samples for chirp sync pulse
-t = [0:7999]/fs;
+
+% GENERATE CHIRP SIGNAL FOR SYNCHRONIZATION
+N = 8000;  %number of samples for chirp
+t = (0:7999)/fs;  % time samples for chirp
 f0 = 300;
 f1 = 3000;
-T = 1;
-c = (f1 - f0)/T;
-x = (c/2)*t.^2 + f0*t;
-pream = sin(2*pi*x);
-pream = pream / max(pream);
+% f_i = f0 + (f1-f0)*t;  % instantaneous frequency of chirp
+pha_i = 2*pi*f0*t + pi*(f1-f0)*t.^2;  % instantaneous phase of chirp (2pi times integral of instantaneous freq)
+chirp = sin(pha_i);  % generate chirp
 
 
+% GENERATE VECTORS OF SILENCE
+guardN = 512;  % number of samples to delay after chirp
+guard = zeros(1,guardN);  % delay of silence after chirp
 
-
-
-
-% samples from sync pulse to ofdm data
-guardN = 512;
-
-
-td = 2;
+td = 2; % number of seconds for silence at beginning and end of audio file
 tdsamples = round(td * fs);
 tdvec = zeros(1,tdsamples);
 
 
-Nchar = 256;  % make power of 2,  number of char for text
-bits = Nchar * 8;
-N2 = bits/2;
-Nfft = bits + 1024;  % bits plus guard freq bins
+% DEFINE VARIABLES
+Nchar = 256;  % number of characters, make power of 2
+Nbits = Nchar * 8;  % number of bits
+Nfft_2 = Nbits + 1024;  % half of length of FFT, bits plus guard freq bins
 
-% get message from txt file
-fid = fopen('C:\SDR_Work\textfile.txt');
+
+% GET MESSAGE FROM TXT FILE, CONVERT TO BITS
+fid = fopen('textfile.txt');
 mtxt = fread(fid);
 fclose(fid);
-mtxt = mtxt';
+mtxt = mtxt'; % convert column vector of text to row vector
+
+% force message to 256 characters by adding or deleting characters
 if length(mtxt) >= Nchar
     mtxt = mtxt(1:Nchar);
 else
@@ -46,11 +44,13 @@ else
     z = Nchar - z;
     mtxt = [mtxt zeros(1,z)];
 end
-mbits = dec2bin(mtxt,8);
-mbits = reshape(mbits',1,[]);
-mbits = mbits(1:bits);
-txbits = zeros(1,bits);
-for k = 1:bits
+
+mbits = dec2bin(mtxt,8); % convert each message character to binary
+mbits = reshape(mbits',1,[]); % rearrange mbits char array to single element
+
+% create vector of bits to transmit based on mbits char element
+txbits = zeros(1,Nbits);
+for k = 1:Nbits
     if mbits(k) == '1'
         txbits(k) = 1;
     else
@@ -59,95 +59,88 @@ for k = 1:bits
 end
 
 
+% MAKE PILOT VECTOR OF RANDOM PHASE ROTATIONS FOR THE FOURIER COEFFICIENTS
+xp = randn(1,Nfft_2);  % vector of length Nfft_2 with normally distributed random numbers
+xp = xp / max(xp) * 12;  % normalize to maximum of 12
+xp = exp(1i*xp);  % create Fourier coefficients for pilot vector
+xp(1:512) = 0; xp(end-511:end) = 0; % zero out first 512 and last 512 coefficients
 
 
-% make pilot vector
-xp = randn(1,Nfft);
-xp = xp / max(xp);
-xp = xp * 12;
-
-
-% pilot vector with 512 guard on each side
-xp = exp(1i*xp);
-xp(1:512) = 0;
-xp(end-511:end) = 0;
-
-% make data vector bpsk,  1 = 1,  0 = -1
+% MAKE BPSK DATA VECTOR,  1 = 1,  0 = -1
 xd = txbits;  % put binary vector here N = 2048 bits
 xd(xd == 0) = -1;
 xd = [zeros(1,512)   xd    zeros(1,512) ];  %512 guard on each side
 
 
+% RELATE DATA VECTOR TO PILOT VECTOR
+xd = xp .* xd;
 
 
+% PREPARE PILOT AND DATA VECTORS FOR IFFT
+xp1 = conj( xp(end:-1:2)); % reverse and conjugate pilot vector
+xp = [xp1 xp]; % concatenate reverse-conjugate pilot with pilot (to make IFFT purely real)
+
+xd1 = conj(xd(end:-1:2)); % reverse and conjugate data vector
+xd = [xd1 xd]; % concatenate reverse-conjugate data with data (to make IFFT purely real)
 
 
-% relate data vector to pilot vector
-xd = xp .* xd;  % e^a * e^b =  e^(a + b)
+% GENERATE PILOT TIME VECTOR
+xp = ifftshift(xp);  % rearrage zero-shifted Fourier coeffients to standard format for IFFT
+xp = ifft(xp);  % IFFT to synthesize time-domain pilot vector
+xp = xp / max(abs(xp));  % normalize to 1
 
 
+% GENERATE DATA TIME VECTOR
+xd = ifftshift(xd); % rearrage zero-shifted Fourier coeffients to standard format for IFFT
+xd = ifft(xd);  % IFFT to synthesize time-domain data vector
+xd = xd / max(abs(xd));  % normalize to 1
 
 
-xp1 = conj( xp(end:-1:2));
-xp = [xp1 xp];
-
-xd1 = conj(xd(end:-1:2));
-xd = [xd1 xd];
-
-
-% pilot time vector
-xp = ifftshift(xp);
-xp = ifft(xp);
-xp = xp / max(abs(xp));
-
-% data time vector
-xd = ifftshift(xd);
-xd = ifft(xd);
-xd = xd / max(abs(xd));
-
-% put the whole movie together
-xz = zeros(1,guardN);
-xt = [tdvec pream xz xp xp xd xd tdvec];
-
-% ensure bandwidth, reduce out of band signals
-hlpf = fir1(64,0.9);
-xt = filter(hlpf,1,xt);
-xt = xt / max(abs(xt));  % 1xN
-
-datafile = [ real(xt).' ];
+% CONCATENATE TIME SIGNALS
+xt = [tdvec chirp guard xp xp xd xd tdvec];
+% note: pilot and data signal are each repeated, representing an extreme case of cyclic prefixing
+% cyclic prefixing provides stronger immunity from errors due to multipath and imprecise synchronization
 
 
+% FILTER OUTPUT TO ENSURE BANDWIDTH, REDUCE OUT OF BAND SIGNALS
+hlpf = fir1(64,0.9);  % FIR filter impulse response, 64th order, low-pass with cutoff of 0.9 rad/sample (3.6 kHz)
+xt = conv(hlpf,xt);  % convolve signal with FIR filter impulse response
+xt = xt / max(abs(xt));  % normalize to 1
 
- audiowrite('c:\SDR_Work\ofdmtest256Char8khzAudio.wav',datafile,fs,'BitsPerSample',16);
-% 
+
+% WRITE TO FILE
+datafile = real(xt).';  % convert time signal from row vector to column vector for audiowrite
+audiowrite('ofdmtest256Char8khzAudio.wav',datafile,fs,'BitsPerSample',16);  % write audio file
 
 
-
-figure(12)
+% PLOT TRANSMITTED SIGNAL
+figure(1)
 plot(real(xt))
 hold on
 plot(imag(xt))
 hold off
-% 
-% figure(13)
-% plot(20*log10(abs(xt)))
-% 
-% 
-% 
-% 
+title('Transmitted Signal')
+xlabel('Sample')
+ylabel('x[n]')
+legend('real','imag')
+
+
+% PLOT MAGNITUDE SPECTRUM OF PILOT
 Xp = fft(xp);
 Xp = Xp( 1:floor(end/2) );
 
-figure(56)
+figure(2)
 plot(abs(Xp))
+title('Magnitude Spectrum of Pilot')
+xlabel('Fourier coefficient')
 
 Xd = fft(xd);
 Xd = Xd( 1:floor(end/2) );
 
 
-det = angle(Xp ./ Xd );
+det = angle(Xp ./ Xd ); % phase difference between pilot and data
 
- det = abs(det);
+det = abs(det);
 n = length(det);
 f = linspace(0,fs/2,n);
 
@@ -156,20 +149,17 @@ thres = pi/2;
 % det = ( det >= thres) .* 1   +  (det < thres) .* 0;
 
 
-figure(34)
+figure(3)
 plot(f,det)
 title('phase results')
+xlabel('frequency')
+ylabel('phase')
 
 
 bits = det(513:end-511);
 
-figure()
+figure(4)
 plot(bits)
-
-
-
-
-
-
-
-
+title('bits')
+xlabel('bit number')
+ylabel('bit phase')
